9156a7ff4b954a7402fd0e3ec322cde07a7fe008
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-10 15:59:01 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Fix Render postgres URL
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL not set")
 
 engine = create_engine(
     DATABASE_URL,
